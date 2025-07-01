26ca740544e8d6a8f9abe0f76ac48e94297cfe30
Saurav <jhasaurav593@gmail.com>
2025-07-01 13:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let scrollLeft;
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
 });
 
@@ -23,8 +23,9 @@ slider.addEventListener('mouseup', () => {
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
+
   e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // scroll-fast multiplier
+  const x = e.pageX;
+  const walk = (x - startX) * 2; // 🔁 speed multiplier
   slider.scrollLeft = scrollLeft - walk;
 });
